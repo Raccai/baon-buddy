f15c4666e9f99eb761e9a2b7583e0c1bc9f5be4b
Raccai <jose.laurel@obf.ateneo.edu>
2025-04-07 17:16:55 +0800

    ADD: added code to App.App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,32 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import BaonCard from './components/BaonCard.svelte'
+  import Navbar from './components/Navbar.svelte'
+  import TalaQuote from './components/TalaQuote.svelte'
+  import { meals } from "./lib/meals.js"
+
+  let suggestedMeals = [];
+
+  function generateMeals() {
+    suggestedMeals = [...meals]
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 3);
+  }
+
+  // generates meals on first load
+  generateMeals();
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1 class="main-title">Baon Buddy</h1>
+  <TalaQuote />
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {each suggestedMeals as meal (meal.name)}
+  <BaonCard {meal} />
+  {/each}
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+<Navbar />
+
+<style lang="css">
+  
 </style>
